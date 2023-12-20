0a81b72231baf3d0939d71678f21e737220b9f15
Meesa000 <asim_99@hotmail.co.uk>
2023-12-20 20:15:12 +0000

    initial

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// CREATE a number variable by dynamically creating a div under number-container
+
+// GET inner HTML and change value to 0 integer
+
+// CREATE a function called RESET
+    // ONCLICK change number variable inner HTML to 0
+
+// CREATE a function called INCREMENT
+    // ONCLICK add one to number variable inner HTML
+
+// CREATE a function called DECREMENT
+    // ONCLICK decrease one to number variable inner HTML
