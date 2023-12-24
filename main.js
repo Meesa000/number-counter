49f966e0620ec5083f5a42ef5b873cf08726c4c1
Meesa000 <asim_99@hotmail.co.uk>
2023-12-24 21:51:22 +0000

    add logic to increase button and change inner html every time its pressed

--- main.js
+++ main.js
@@ -1,26 +1,59 @@
 
+let counter = 0;
+
+
 // CREATE a number variable by dynamically creating a div under number-container
 
 // REFERENCE the number container
 let numberContainer = document.getElementById('number-container');
 console.log(numberContainer)
+
 // CREATE new div dynamically called number
 let number = document.createElement('div');
 console.log(number)
 // SET number div content 
-number.innerHTML = 'Test';
+number.innerHTML = counter;
+
 // APPEND new number div to the number container referenced earlier
 numberContainer.appendChild(number);
 
+// Buttons references
+let increaseButton = document.querySelector('.increase-btn');
+console.log(increaseButton);
 
 
-// GET inner HTML and change value to 0 integer
+function increment() {
+    counter = counter +1;
+    number.innerHTML = counter;
+    console.log('Increase button clicked!');
+    console.log('Counter: ' + counter)
+    
+}
+
+increaseButton.addEventListener('click', increment);
+
+function decrement() {
+    counter = counter -1;
+    number.innerHTML = counter;
+    console.log('Counter: ' + counter);
+}
+
+
+
+
+
+
+
+
+// CREATE a function called INCREMENT
+    // ONCLICK add one to number variable inner HTML
+
+
 
 // CREATE a function called RESET
     // ONCLICK change number variable inner HTML to 0
 
-// CREATE a function called INCREMENT
-    // ONCLICK add one to number variable inner HTML
+
 
 // CREATE a function called DECREMENT
     // ONCLICK decrease one to number variable inner HTML
